aa054bf81bbe20128ae6d7b6e00b6c7473106e48
Brad <bgreen3@uoregon.edu>
2017-02-27 11:48:16 -0700

    Add minor styling

--- spec/features/links/create_link_spec.rb
+++ spec/features/links/create_link_spec.rb
@@ -1,16 +1,21 @@
 require "rails_helper"
 
-RSpec.describe "can create links", :js => :true do
-  scenario "Create a new link" do
-    visit "/"
-    fill_in "Title:", :with => "Turing"
-    fill_in "URL:", :with => "http://turing.io"
-    click_on "Add Link"
-
-    within('#links-list') do
-      expect(page).to have_text("Turing")
-      expect(page).to have_text("http://turing.io")
+RSpec.describe "create links", :js => :true do
+  scenario "an unauthenticated user" do
+    it "cannot submit a link" do
+      expect(page).to_not contain("Add Link")
     end
-
   end
-end
+  scenario "an authenticated user" do
+    it "can add a link" do
+      visit "/"
+      fill_in "Title:", :with => "Turing"
+      fill_in "URL:", :with => "http://turing.io"
+      click_on "Add Link"
+
+      within('#links-list') do
+        expect(page).to have_text("Turing")
+        expect(page).to have_text("http://turing.io")
+      end
+    end
+  end
