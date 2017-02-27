8b343fcda087031744bfb6ca24784404fe9c9d9c
Brad <bgreen3@uoregon.edu>
2017-02-27 15:43:45 -0700

    Add testing for edit links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,4 +3,28 @@ class LinksController < ApplicationController
     @hot_links = Link.hot
     @links = Link.where(user_id: current_user.id) if current_user
   end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      flash[:success] = "Updated!"
+      redirect_to links_path
+    else
+      @link.errors.full_messages.each do |message|
+        flash[:alert] = message
+      end
+      render :edit
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :title)
+  end
+
 end
