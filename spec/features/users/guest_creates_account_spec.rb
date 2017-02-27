5dbdb627db93040eff28142b82370ad220535682
Brad <bgreen3@uoregon.edu>
2017-02-27 10:46:40 -0700

    Add sad path testing for user sign up

--- spec/features/users/guest_creates_account_spec.rb
+++ spec/features/users/guest_creates_account_spec.rb
@@ -17,3 +17,35 @@ context "a not logged in user" do
 
     end
   end
+  context "a not logged in user" do
+    it "cannot create account with email already in use" do
+
+      User.create(email: "brad@test.com", password: "pass")
+
+      visit signup_path
+
+      fill_in 'user_email', with: "brad@test.com"
+      fill_in 'user_password', with: "pass"
+      fill_in 'user_password_confirmation', with: "pass"
+
+      click_button "Submit"
+
+      expect(current_path).to eq(signup_path)
+      expect(page).to have_content("Email has already been taken")
+    end
+  end
+  context "a not logged in user" do
+    it "cannot create account with if password confirmation doesn't match password" do
+
+      visit signup_path
+
+      fill_in 'user_email', with: "brad@test.com"
+      fill_in 'user_password', with: "pass"
+      fill_in 'user_password_confirmation', with: "password"
+
+      click_button "Submit"
+
+      expect(current_path).to eq(signup_path)
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+  end
