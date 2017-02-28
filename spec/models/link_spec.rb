d35c5594c19bcc952dc713c119b1b3f6e4ddbc90
Brad <bgreen3@uoregon.edu>
2017-02-28 03:05:51 -0700

    Add unit test for search

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Read, type: :model do
+RSpec.describe Link, type: :model do
   it "is not created with missing suffix" do
     link = Link.new(url: 'www.test', title: 'Test')
     expect(link).to_not be_valid
@@ -24,5 +24,17 @@ RSpec.describe Read, type: :model do
   it "is created with www prefix" do
     link = Link.new(url: 'www.test.com', title: 'Test')
     expect(link).to be_valid
+  end
+  it "returns appropriate results by title" do
+    link_1 = Link.create(title: "Google", url: "www.google.com")
+    link_2 = Link.create(title: "Gong", url: "www.gong.com")
+    link_3 = Link.create(title: "ZZZ", url: "www.zzz.com")
+
+    results = Link.search("Go")
+
+    expect(results.count).to eq(2)
+    result.each do |result|
+      expect(result.title).to_not eq("ZZZ")
+    end
   end
 end
