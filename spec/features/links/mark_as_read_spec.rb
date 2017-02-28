8a80408d8732f6d3f2c36848b2dffd6e3721448f
Brad <bgreen3@uoregon.edu>
2017-02-27 17:42:43 -0700

    Add test for marking link as read

---
+++ spec/features/links/mark_as_read_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "mark links as read", :js => :true do
+  context "an authenticated user marking a link as read" do
+    it "sees read status change from false to true" do
+      login_user
+      link = Link.create(title: "Google", url: "www.google.com", user_id: User.first.id)
+
+      visit root_path
+
+      expect(page).to have_content("false")
+
+      click_on("Mark as Read")
+
+      expect(page).to have_content("true")
+      expect(page).to_not have_content("false")
+    end
+  end
+end
