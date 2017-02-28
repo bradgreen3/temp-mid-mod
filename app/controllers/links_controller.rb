3bae9396c67811b9b0dddb198406028fb9d88d91
Brad <bgreen3@uoregon.edu>
2017-02-27 18:18:13 -0700

    Add CORS gem and Ajax call to Hot Reads

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,8 @@
 class LinksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    @hot_links = Link.hot
+    # @hot_links = Link.hot
     @links = Link.where(user_id: current_user.id) if current_user
   end
 
